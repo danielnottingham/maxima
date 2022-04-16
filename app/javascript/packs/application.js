7594ea5fe0edcae6ce86e2258eddf4da3e0d5653
“Marcos <marcos.caliope@gmail.com>
2022-04-16 16:56:24 -0300

    navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require rails_ujs
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -9,9 +13,6 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 
-//= require jquery3
-//= require popper
-//= require bootstrap
 
 import "bootstrap"
 
